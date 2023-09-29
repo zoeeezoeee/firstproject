630c2ceffcc50f9aa62f422f278ef54b3b5d5e65
zoeeezoeee <141098145+zoeeezoeee@users.noreply.github.com>
2023-09-29 11:30:32 +1000

    2

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const colors = ['red','blue','red','yellow','pink','green'];
+const colors = ['red','blue','purpel','yellow','pink','green'];
 
 
 const container = document.querySelector('#boxes');
@@ -6,7 +6,16 @@ for(let color of colors){
     const box = document.createElement('div');
 
     box.style.backgroundColor = color;
+    //const neveshte= document.creatElement('h3');
+   // neveshte.innertext= "funny";
     
     box.classList.add('box');
     container.appendChild(box);
+    box.addEventListener('click', function(){
+       console.log("click me ");
+
+    })
 }  
+
+
+
